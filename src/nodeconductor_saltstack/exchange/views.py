da5df30388c60465370309fca353ca6b210f7ec0
Roman Kosenko <Roman.Kosenko@gmail.com>
2015-12-23 09:25:42 +0000

    Base class for views

--- src/nodeconductor_saltstack/exchange/views.py
+++ src/nodeconductor_saltstack/exchange/views.py
@@ -1,10 +1,7 @@
-from rest_framework import exceptions, filters, permissions, viewsets
-
 from nodeconductor.core.exceptions import IncorrectStateException
-from nodeconductor.structure.filters import GenericRoleFilter
 from nodeconductor.structure import views as structure_views
 
-from ..saltstack.backend import SaltStackBackendError
+from ..saltstack.views import BasePropertyViewSet, track_exceptions
 from .filters import UserFilter
 from . import models, serializers
 
@@ -18,25 +15,20 @@ class TenantViewSet(structure_views.BaseOnlineResourceViewSet):
         backend = resource.get_backend()
         backend.provision(resource)
 
+    @track_exceptions
     def perform_update(self, serializer):
         tenant = self.get_object()
         backend = tenant.get_backend()
-        try:
-            backend.tenants.change(domain=serializer.validated_data['domain'])
-        except SaltStackBackendError as e:
-            raise exceptions.APIException(e.traceback_str)
-        else:
-            serializer.save()
+        backend.tenants.change(domain=serializer.validated_data['domain'])
+        serializer.save()
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(BasePropertyViewSet):
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
-    lookup_field = 'uuid'
-    permission_classes = (permissions.IsAuthenticated, permissions.DjangoObjectPermissions)
-    filter_backends = (GenericRoleFilter, filters.DjangoFilterBackend,)
     filter_class = UserFilter
 
+    @track_exceptions
     def perform_create(self, serializer):
         tenant = serializer.validated_data['tenant']
         backend = tenant.get_backend()
@@ -44,11 +36,8 @@ class UserViewSet(viewsets.ModelViewSet):
         if tenant.state != models.ExchangeTenant.States.ONLINE:
             raise IncorrectStateException("Tenant must be in stable state to perform user creation")
 
-        try:
-            backend_user = backend.users.create(
-                **{k: v for k, v in serializer.validated_data.items() if k not in ('tenant',)})
-        except SaltStackBackendError as e:
-            raise exceptions.APIException(e.traceback_str)
+        backend_user = backend.users.create(
+            **{k: v for k, v in serializer.validated_data.items() if k not in ('tenant',)})
 
         user = serializer.save()
         user.email = backend_user.email
@@ -59,15 +48,12 @@ class UserViewSet(viewsets.ModelViewSet):
         user.tenant.add_quota_usage('user_count', 1)
         user.tenant.add_quota_usage('global_mailbox_size', user.mailbox_size)
 
+    @track_exceptions
     def perform_update(self, serializer):
         user = self.get_object()
         backend = user.tenant.get_backend()
-        try:
-            changed = {k: v for k, v in serializer.validated_data.items() if v and getattr(user, k) != v}
-            backend.users.change(id=user.backend_id, **changed)
-        except SaltStackBackendError as e:
-            raise exceptions.APIException(e.traceback_str)
-
+        changed = {k: v for k, v in serializer.validated_data.items() if v and getattr(user, k) != v}
+        backend.users.change(id=user.backend_id, **changed)
         serializer.save()
 
         user.tenant.add_quota_usage(
@@ -75,10 +61,7 @@ class UserViewSet(viewsets.ModelViewSet):
 
     def perform_destroy(self, user):
         backend = user.tenant.get_backend()
-        try:
-            backend.users.delete(id=user.backend_id)
-        except SaltStackBackendError as e:
-            raise exceptions.APIException(e.traceback_str)
+        backend.users.delete(id=user.backend_id)
 
         user.delete()
         user.tenant.add_quota_usage('user_count', -1)
--- src/nodeconductor_saltstack/saltstack/views.py
+++ src/nodeconductor_saltstack/saltstack/views.py
@@ -1,5 +1,10 @@
+from functools import wraps
+from rest_framework import exceptions, filters, permissions, viewsets
+
+from nodeconductor.structure.filters import GenericRoleFilter
 from nodeconductor.structure import views as structure_views
 
+from .backend import SaltStackBackendError
 from . import models, serializers
 
 
@@ -11,3 +16,23 @@ class SaltStackServiceViewSet(structure_views.BaseServiceViewSet):
 class SaltStackServiceProjectLinkViewSet(structure_views.BaseServiceProjectLinkViewSet):
     queryset = models.SaltStackServiceProjectLink.objects.all()
     serializer_class = serializers.ServiceProjectLinkSerializer
+
+
+class BasePropertyViewSet(viewsets.ModelViewSet):
+    queryset = NotImplemented
+    serializer_class = NotImplemented
+    lookup_field = 'uuid'
+    permission_classes = (permissions.IsAuthenticated, permissions.DjangoObjectPermissions)
+    filter_backends = (GenericRoleFilter, filters.DjangoFilterBackend,)
+
+
+def track_exceptions():
+    def decorator(view_fn):
+        @wraps(view_fn)
+        def wrapped(self, *args, **kwargs):
+            try:
+                view_fn(self, *args, **kwargs)
+            except SaltStackBackendError as e:
+                raise exceptions.APIException(e.traceback_str)
+        return wrapped
+    return decorator
