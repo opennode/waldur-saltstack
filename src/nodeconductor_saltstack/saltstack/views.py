3af85ac120b5bd745fb150c810ab3ad96db1ba44
Roman Kosenko <Roman.Kosenko@gmail.com>
2016-02-03 14:27:29 +0000

    Refactor Exchange group update method

--- src/nodeconductor_saltstack/saltstack/views.py
+++ src/nodeconductor_saltstack/saltstack/views.py
@@ -71,7 +71,9 @@ class BasePropertyViewSet(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         obj = self.get_object()
         backend = self.get_backend(obj.tenant)
-        changed = {k: v for k, v in serializer.validated_data.items() if v and getattr(obj, k) != v}
+        changed = {
+            k: v for k, v in serializer.validated_data.items()
+            if v and k in backend.Methods.create['input'] and getattr(obj, k) != v}
         if changed:
             backend.change(id=obj.backend_id, **changed)
         serializer.save()
