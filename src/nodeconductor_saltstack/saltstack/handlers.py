b3e02aca4b7ad1cb4395b25b7326f869ae6d6428
Ilja Livenson <ilja@opennodecloud.com>
2016-02-22 14:12:41 +0400

    Add logging of CUD for SaltStack properties
    - ITACLOUD-6292

---
+++ src/nodeconductor_saltstack/saltstack/handlers.py
@@ -0,0 +1,39 @@
+from __future__ import unicode_literals
+
+import logging
+from nodeconductor.structure import SupportedServices
+
+from ..saltstack.log import event_logger
+
+
+logger = logging.getLogger(__name__)
+
+
+def log_saltstack_property_created(sender, instance, created=False, **kwargs):
+    property_type = SupportedServices._get_model_str(instance).replace('.', ' ').capitalize()
+    if created:
+        event_logger.saltstack_property.info(
+            '%s {property_name} has been created.' % property_type,
+            event_type='saltstack_property_creation_succeeded',
+            event_context={
+                'property': instance,
+            })
+    else:
+        event_logger.saltstack_property.info(
+            '%s {property_name} has been updated.' % property_type,
+            event_type='saltstack_property_creation_succeeded',
+            event_context={
+                'property': instance,
+            })
+
+
+def log_saltstack_property_deleted(sender, instance, **kwargs):
+    property_type = SupportedServices._get_model_str(instance).replace('.', ' ').capitalize()
+    event_logger.saltstack_property.info(
+        '%s {property_name} has been deleted.' % property_type,
+        event_type='saltstack_property_deletion_succeeded',
+        event_context={
+            'property': instance,
+        })
+
+
