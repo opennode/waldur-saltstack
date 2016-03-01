ea027380f361336702e8abafeeddbd1f1b57b852
Ilja Livenson <ilja@opennodecloud.com>
2016-03-01 10:55:19 +0400

    Fix event log names

--- src/nodeconductor_saltstack/saltstack/handlers.py
+++ src/nodeconductor_saltstack/saltstack/handlers.py
@@ -10,14 +10,14 @@ logger = logging.getLogger(__name__)
 def log_saltstack_property_created(sender, instance, created=False, **kwargs):
     if created:
         event_logger.saltstack_property.info(
-            '%s {property_name} has been created in {tenant_full_name}.' % instance.get_type_display_name(),
+            '%s {property_name} has been created in {resource_full_name}.' % instance.get_type_display_name(),
             event_type='saltstack_property_creation_succeeded',
             event_context={
                 'property': instance,
             })
     else:
         event_logger.saltstack_property.info(
-            '%s {property_name} has been updated in {tenant_full_name}.' % instance.get_type_display_name(),
+            '%s {property_name} has been updated in {resource_full_name}.' % instance.get_type_display_name(),
             event_type='saltstack_property_update_succeeded',
             event_context={
                 'property': instance,
@@ -26,7 +26,7 @@ def log_saltstack_property_created(sender, instance, created=False, **kwargs):
 
 def log_saltstack_property_deleted(sender, instance, **kwargs):
     event_logger.saltstack_property.info(
-        '%s {property_name} has been deleted from {tenant_full_name}.' % instance.get_type_display_name(),
+        '%s {property_name} has been deleted from {resource_full_name}.' % instance.get_type_display_name(),
         event_type='saltstack_property_deletion_succeeded',
         event_context={
             'property': instance,
