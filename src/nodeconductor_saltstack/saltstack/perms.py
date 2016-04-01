501ede985c034f4883ac93a38f8486af6fddf766
Victor Mireyev <victor@opennodecloud.com>
2016-04-01 12:31:05 +0300

    Make permissions declaration DRY (NC-1282)

--- src/nodeconductor_saltstack/saltstack/perms.py
+++ src/nodeconductor_saltstack/saltstack/perms.py
@@ -1,36 +1,9 @@
-from nodeconductor.core.permissions import FilteredCollaboratorsPermissionLogic
-from nodeconductor.structure.models import CustomerRole, ProjectGroupRole, ProjectRole
+from nodeconductor.structure import perms as structure_perms
 
 
 PERMISSION_LOGICS = (
-    ('saltstack.SaltStackService', FilteredCollaboratorsPermissionLogic(
-        collaborators_query='customer__roles__permission_group__user',
-        collaborators_filter={
-            'customer__roles__role_type': CustomerRole.OWNER,
-        },
-        any_permission=True,
-    )),
-    ('saltstack.SaltStackServiceProjectLink', FilteredCollaboratorsPermissionLogic(
-        collaborators_query=[
-            'service__customer__roles__permission_group__user',
-            'project__project_groups__roles__permission_group__user',
-        ],
-        collaborators_filter=[
-            {'service__customer__roles__role_type': CustomerRole.OWNER},
-            {'project__project_groups__roles__role_type': ProjectGroupRole.MANAGER},
-        ],
-        any_permission=True,
-    )),
+    ('saltstack.SaltStackService', structure_perms.service_permission_logic),
+    ('saltstack.SaltStackServiceProjectLink', structure_perms.service_project_link_permission_logic),
 )
 
-property_permission_logic = FilteredCollaboratorsPermissionLogic(
-    collaborators_query=[
-        'tenant__service_project_link__project__roles__permission_group__user',
-        'tenant__service_project_link__project__customer__roles__permission_group__user',
-    ],
-    collaborators_filter=[
-        {'tenant__service_project_link__project__roles__role_type': ProjectRole.ADMINISTRATOR},
-        {'tenant__service_project_link__project__customer__roles__role_type': CustomerRole.OWNER},
-    ],
-    any_permission=True,
-)
+property_permission_logic = structure_perms.property_permission_logic('tenant')
